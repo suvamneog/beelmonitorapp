657452eb62d458aa89032c36da4c51e415ce66c7
suvamneog <neogsuvam@gmail.com>
2025-07-29 14:53:42 +0530

    ADDED

--- client/app.config.js
+++ client/app.config.js
@@ -1,9 +1,1 @@
-import 'dotenv/config';
-
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    ...config.extra,
-    apiBaseUrl: process.env.BASE_URL,
-  },
-});
+export default ({ config }) => config; 
